9e0516b9e4c4771a62087a5c03ac66391039e568
Laurent Constantin <constantin.laurent@gmail.com>
2023-01-10 22:35:30 +0100

    Fix ParseResult for phpstan

--- src/Entity/ParseResult.php
+++ src/Entity/ParseResult.php
@@ -2,13 +2,16 @@
 
 namespace App\Entity;
 
-class ParseResult implements \ArrayAccess, \Countable
+/**
+ * @template T
+ */
+class ParseResult extends \ArrayIterator implements \iterable
 {
     private array $extra;
 
     public function __construct(array $data, array $extra = [])
     {
-        $this->data = $data;
+        parent::__construct($data);
         $this->extra = $extra;
     }
 
@@ -20,30 +23,5 @@ class ParseResult implements \ArrayAccess, \Countable
     public function getExtra(object $object, string $name): mixed
     {
         return $this->extra[spl_object_hash($object)][$name] ?? null;
-    }
-
-    public function offsetExists(mixed $offset): bool
-    {
-        return array_key_exists($offset, $this->data);
-    }
-
-    public function offsetGet(mixed $offset): mixed
-    {
-        return $this->data[$offset];
-    }
-
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        $this->data[$offset] = $value;
-    }
-
-    public function offsetUnset(mixed $offset): void
-    {
-        unset($this->data[$offset]);
-    }
-
-    public function count(): int
-    {
-        return count($this->data);
     }
 }
