42f2c4357dae02bbeaf5fceceaacf9cbe2d49980
Laurent Constantin <constantin.laurent@gmail.com>
2023-01-20 01:25:29 +0100

    Timestamp on invoices

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -5,12 +5,14 @@ namespace App\Entity;
 use App\Repository\InvoiceRepository;
 use App\Trait\PriceEntityTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: InvoiceRepository::class)]
 #[ORM\HasLifecycleCallbacks]
 class Invoice
 {
     use PriceEntityTrait;
+    use TimestampableEntity;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
