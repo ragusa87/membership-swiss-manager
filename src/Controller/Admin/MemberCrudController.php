f428573a1aa722790ab67f8533a2e23e8e64f029
Laurent Constantin <constantin.laurent@gmail.com>
2023-01-20 11:44:53 +0100

    Improve members admin and dashboard

--- src/Controller/Admin/MemberCrudController.php
+++ src/Controller/Admin/MemberCrudController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Member;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -29,14 +31,25 @@ class MemberCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield TextField::new('firstname')->setLabel("firstname from $pageName");
-        yield TextField::new('lastname');
-        yield EmailField::new('email');
+        yield TextField::new('firstname')->setFormTypeOption('attr', ['placeholder' => 'William']);
+        yield TextField::new('lastname')->setFormTypeOption('attr', ['placeholder' => 'Shakespeare']);
+        yield EmailField::new('email')->setFormTypeOption('attr', ['placeholder' => 'email@example.com']);
         yield TextEditorField::new('comment');
+
+        yield AssociationField::new('parent')
+            ->setRequired(false);
+
+        if (in_array($pageName, ['new', 'edit'])) {
+            yield TextField::new('address')->setRequired(false)->setFormTypeOption('attr', ['placeholder' => 'Chemin du Vanil']);
+            yield TextField::new('address_number')->setRequired(false)->setFormTypeOption('attr', ['placeholder' => '10']);
+            yield TextField::new('city')->setRequired(false)->setFormTypeOption('attr', ['placeholder' => 'Lausanne']);
+            yield IntegerField::new('zip')->setRequired(false)->setFormTypeOption('attr', ['placeholder' => '1006']);
+            yield TextField::new('phone')->setRequired(false)->setFormTypeOption('attr', ['placeholder' => '+41 7x xxx xx xx']);
+        }
 
         if ('edit' !== $pageName && 'index' !== $pageName) {
             yield CollectionField::new('memberSubscription')
-                ->setLabel('memberSubscription while on page '.$pageName)
+                ->setLabel('memberSubscription')
                 ->useEntryCrudForm(null, $pageName.'-new-from-member', $pageName.'-edit-from-member')
                 ->setEntryIsComplex(true)
                 ->renderExpanded(true)
