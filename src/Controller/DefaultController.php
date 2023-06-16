c7a1b14fca1f2056aa556cc2e8930e4785c11c41
Laurent Constantin <laurent.constantin@liip.ch>
2023-06-16 11:25:08 +0200

    Improve CAMT interface and pay feature

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Controller\Admin\DashboardController;
 use App\Controller\Admin\InvoiceCrudController;
+use App\Entity\InvoiceStatusEnum;
+use App\Repository\InvoiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -40,5 +44,36 @@ class DefaultController extends AbstractController
             ->generateUrl();
 
         return $this->redirect($url);
+    }
+
+    #[Route(path: '/pay-invoice-id/{id}', name: 'pay_invoice_by_id')]
+    public function payInvoicesById(int $id, Request $request, EntityManagerInterface $em, InvoiceRepository $invoiceRepository)
+    {
+        $invoice = $invoiceRepository->find($id);
+        if (null === $invoice) {
+            throw $this->createNotFoundException('Invoice not found');
+        }
+        if (InvoiceStatusEnum::PAID === $invoice->getStatusAsEnum()) {
+            throw $this->createAccessDeniedException('Invoice already paid');
+        }
+
+        $status = $request->query->get('status');
+        if (false === in_array($status, [null, 'paid'], true)) {
+            throw $this->createAccessDeniedException('Unsupported status '.$status);
+        }
+
+        $invoice->setStatusFromEnum(InvoiceStatusEnum::PAID);
+
+        $price = $request->query->getInt('amount');
+
+        if (0 !== $price) {
+            $invoice->setPrice($price);
+        }
+
+        $invoice->setUpdatedAt(new \DateTime());
+        $em->persist($invoice);
+        $em->flush();
+
+        return $this->redirect($request->headers->get('referer') ?? $this->generateUrl('index'));
     }
 }
